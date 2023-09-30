b14eb22696ed29204eb455faa8dba71c298ab173
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-09-30 12:25:53 +0600

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,54 @@
-import "./Navbar"
+import "./navbar.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import BedtimeOutlinedIcon from '@mui/icons-material/BedtimeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import TocOutlinedIcon from '@mui/icons-material/TocOutlined';
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Navbar
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..."/>
+          <SearchIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+          <LanguageIcon className="icon"/>
+          English
+          </div>
+          <div className="item">
+          <BedtimeOutlinedIcon className="icon"/>
+          
+          </div>
+          <div className="item">
+          <FullscreenExitOutlinedIcon className="icon"/>
+          
+          </div>
+          <div className="item">
+          <NotificationsNoneOutlinedIcon className="icon"/>
+          <div className="counter">1</div>
+          </div>
+          <div className="item">
+          <ChatBubbleOutlineOutlinedIcon className="icon"/>
+          <div className="counter">2</div>
+          </div>
+          <div className="item">
+          <TocOutlinedIcon className="icon"/>
+          
+          </div>
+          <div className="item">
+          
+          <img src="https://cdn.pixabay.com/photo/2013/05/11/08/28/sunset-110305_960_720.jpg" alt="" 
+            className= "avatar"
+          />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
