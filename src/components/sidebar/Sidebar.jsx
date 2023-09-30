53e6f0599b7e45eab9b2530f04c4e08e0acb87ed
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-09-30 10:15:13 +0600

    Sidebar done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,32 +1,106 @@
 import "./sidebar.scss"
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import GradingOutlinedIcon from '@mui/icons-material/GradingOutlined';
+import DeliveryDiningOutlinedIcon from '@mui/icons-material/DeliveryDiningOutlined';
+import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import FolderSharedOutlinedIcon from '@mui/icons-material/FolderSharedOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="top">
-        <span className="logo">hasanadmin</span>
+        <span className="logo">ADMIN</span>
     </div>
     <hr />
       <div className="center">
       <ul>
+      <p className="title">MAIN</p>
         <li>
-              <DashboardIcon/>
+        <span className="icon"><DashboardIcon/></span>
+              
             <span>Dashboard</span>
         </li>
+        <p className="title">LISTS</p>
         <li>
+        <span className="icon"><PersonOutlineOutlinedIcon/></span>
+            
             <span>Users</span>
         </li>
+        
         <li>
+        <span className="icon"><Inventory2OutlinedIcon/></span>
+            
             <span>Products</span>
         </li>
+        <p className="title">MAIN</p>
         <li>
+        <span className="icon"><GradingOutlinedIcon/></span>
+            
             <span>Orders</span>
         </li>
+        
+        <li>
+        <span className="icon"><DeliveryDiningOutlinedIcon/></span>
+            
+            <span>Delivery</span>
+        </li>
+        <p className="title">USEFUL</p>
+        <li>
+        <span className="icon"><QueryStatsOutlinedIcon/></span>
+            
+            <span>Stats</span>
+        </li>
+        
+        <li>
+        <span className="icon"><NotificationsOutlinedIcon/></span>
+            
+            <span>Notification</span>
+        </li>
+        <p className="title">SERVICE</p>
+        <li>
+        <span className="icon"><MonitorHeartOutlinedIcon/></span>
+            
+            <span>System Health</span>
+        </li>
+        
+        <li>
+        <span className="icon"><PsychologyOutlinedIcon/></span>
+            
+            <span>Logs</span>
+        </li>
+        
+        <li>
+        <span className="icon"><SettingsOutlinedIcon/></span>
+            
+            <span>Settings</span>
+        </li>
+        <p className="title">USER</p>
+        <li>
+        <span className="icon"><FolderSharedOutlinedIcon/></span>
+            
+            <span>Profile</span>
+        </li>
+        
+        <li>
+        <span className="icon"><ExitToAppOutlinedIcon/></span>
+            
+            <span>Logout</span>
+        </li>
+        
       </ul>
       </div>
-      <div className="bottom">color option</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
