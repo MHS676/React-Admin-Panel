bae37769f31a22047569f0c0937c7221fd026c7d
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-09-30 17:10:02 +0600

    widget done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,21 @@
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
+import Widget from "../../components/widget/Widget"
 import "./home.scss"
 
 const Home = () => {
+
   return (
     <div className="home">
       <Sidebar/>
       <div className="homeContainer">
         <Navbar/>
-        home container
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
       </div>
     </div>
   )
